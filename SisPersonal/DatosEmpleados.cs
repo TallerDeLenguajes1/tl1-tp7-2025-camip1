ef6240facc0819f2dfa8a03826cb609efdd273af
Perez Milagros Camila <camilap11517@gmail.com>
2025-06-16 14:04:48 -0300

    Ejercicio 2 completo

--- SisPersonal/DatosEmpleados.cs
+++ SisPersonal/DatosEmpleados.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace DatosEmpleados
 {
     public enum Cargo
@@ -55,6 +57,66 @@ namespace DatosEmpleados
             get => cargo;
             set => cargo = value;
         }
+
+        //metodos
+        public int CalcularAntiguedad()
+        {
+            return DateTime.Now.Year - FechaIngreso.Year;
+        }
+        public int ObtenerEdad()
+        {
+            DateTime hoy = DateTime.Today;
+            int edad = hoy.Year - FechaDeNacimiento.Year;
+            if (FechaDeNacimiento.Date > hoy.Date)
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+        public int AñosJubilacion()
+        {
+            int edad = ObtenerEdad();
+            return 65 - edad;
+        }
+
+        public double CalcularSalario()
+        {
+            double Adicional;
+            int antiguedad = CalcularAntiguedad();
+            if (antiguedad <= 20)
+            {
+                Adicional = SueldoBasico * (antiguedad * 0.01);
+            }
+            else
+            {
+                Adicional = SueldoBasico * 0.25;
+            }
+            if (CargoEmpleado == Cargo.Ingeniero || CargoEmpleado == Cargo.Especialista)
+            {
+                Adicional *= 1.50;
+            }
+            if (EstadoCivil == 'C')
+            {
+                Adicional += 150000;
+            }
+            double salario = SueldoBasico + Adicional;
+            return salario;
+        }
+
+        public void MostrarDatosEmpleado()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Apellido: {Apellido}");
+            Console.WriteLine($"Fecha de Nacimiento: {FechaDeNacimiento}");
+            Console.WriteLine($"Edad: {ObtenerEdad()}");
+            Console.WriteLine($"Estado Civil: {EstadoCivil}");
+            Console.WriteLine($"Fecha de ingreso a la empresa: {FechaIngreso}");
+            Console.WriteLine($"Sueldo basico: {SueldoBasico}");
+            Console.WriteLine($"Cargo: {CargoEmpleado}");
+            Console.WriteLine($"Antiguedad en la empresa: {CalcularAntiguedad()}");
+            Console.WriteLine($"Años restantes para la jubilacion: {AñosJubilacion()}");
+            Console.WriteLine($"Salario: {CalcularSalario()}\n");
+        }
     }
 }
-
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,3 +1,62 @@
 ﻿using DatosEmpleados;
 
+Empleado[] empleados = new Empleado[3];
+            
+empleados[0] = new Empleado
+{
+    Nombre = "Adrian",
+    Apellido = "Reyes",
+    FechaDeNacimiento = new DateTime(1971, 9, 8),
+    EstadoCivil = 'C',
+    FechaIngreso = new DateTime(2004, 3, 1),
+    SueldoBasico = 850000,
+    CargoEmpleado = Cargo.Administrativo
+};
 
+empleados[1] = new Empleado
+{
+    Nombre = "Sofia",
+    Apellido = "Juarez",
+    FechaDeNacimiento = new DateTime(2004, 7, 15),
+    EstadoCivil = 'S',
+    FechaIngreso = new DateTime(2020, 5, 12),
+    SueldoBasico = 570000,
+    CargoEmpleado = Cargo.Especialista
+};
+
+empleados[2] = new Empleado
+{
+    Nombre = "Juan",
+    Apellido = "Cordoba",
+    FechaDeNacimiento = new DateTime(1990, 5, 10),
+    EstadoCivil = 'C',
+    FechaIngreso = new DateTime(2010, 3, 1),
+    SueldoBasico = 650000,
+    CargoEmpleado = Cargo.Ingeniero
+};
+
+Console.WriteLine("\nEmpleados\n");
+foreach (var empleado in empleados)
+{
+    empleado.MostrarDatosEmpleado();
+}
+
+Console.WriteLine("Calculo del total abonado en salarios");
+double totalSalarios = 0;
+foreach (var empleado in empleados)
+{
+    totalSalarios += empleado.CalcularSalario();
+}
+Console.WriteLine($"El total pagado en salarios es de {totalSalarios}");
+
+Console.WriteLine("\nDatos del empleado mas proximo a jubilarse\n");
+Empleado proximoJubilacion = empleados[0];
+int indice=0;
+foreach (var empleado in empleados)
+{
+    if (empleado.AñosJubilacion() < proximoJubilacion.AñosJubilacion())
+    {
+        proximoJubilacion = empleado;
+    }
+}
+empleados[indice].MostrarDatosEmpleado();
